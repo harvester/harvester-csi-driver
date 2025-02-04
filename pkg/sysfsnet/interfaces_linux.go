d98eb6f1460790cbfa52d86945cae52c460ecb1b
Ivan Sim <ivan.sim@suse.com>
2025-02-04 14:18:46 -0800

    fix: skip flat files and symlinks in /sys/class/net
    Signed-off-by: Ivan Sim <ivan.sim@suse.com>

--- pkg/sysfsnet/interfaces_linux.go
+++ pkg/sysfsnet/interfaces_linux.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+
+	"github.com/sirupsen/logrus"
 )
 
 var sysClassNet = "/sys/class/net"
@@ -23,9 +25,7 @@ func Interfaces() ([]Interface, error) {
 	ifaces := make([]Interface, 0, 16)
 
 	readMACFromFile := func(s string) (string, error) {
-
 		f, err := os.Open(s)
-
 		if os.IsNotExist(err) {
 			return "", nil
 		}
@@ -41,17 +41,15 @@ func Interfaces() ([]Interface, error) {
 	}
 
 	for _, dentry := range dents {
-
 		entryPath := filepath.Join(sysClassNet, dentry.Name())
-
-		// os.Stat to follow symlinks and return the info of the target
-		info, err := os.Stat(entryPath)
+		dinfo, err := os.Stat(entryPath) // stat follows symlinks to directories
 		if err != nil {
+			logrus.Infof("skipping %s. unexpected error: %s", entryPath, err)
 			continue
 		}
 
-		// os.FileInfo.IsDir to check if the entry is a directory
-		if !info.IsDir() {
+		if !dinfo.IsDir() {
+			logrus.Infof("skipping %s: not a directory", entryPath)
 			continue
 		}
 
--- pkg/sysfsnet/interfaces_test.go
+++ pkg/sysfsnet/interfaces_test.go
@@ -27,32 +27,35 @@ func TestInterfaces(t *testing.T) {
 		}
 	}
 
-	nonDirPath := filepath.Join(sysClassNet, "bonding_masters")
-	if err := os.WriteFile(nonDirPath, []byte(""), 0o644); err != nil {
-		t.Fatalf("os.WriteFile %q want err=<nil>, got err=%v", nonDirPath, err)
+	// Interfaces() should follow symlinkDir.
+	symlinkDir := filepath.Join(sysClassNet, "eth0_ln")
+	if err := os.Symlink(filepath.Join(sysClassNet, "eth0"), symlinkDir); err != nil {
+		t.Fatalf("os.Symlink %q want err=<nil>, got err=%v", symlinkDir, err)
 	}
 
-	ifaces, err := Interfaces()
+	// Interfaces() should skip non-directories like flatFile and symlinkFile.
+	flatFile := filepath.Join(sysClassNet, "bonding_masters")
+	if err := os.WriteFile(flatFile, []byte("bond1 bond2 bond3"), 0o644); err != nil {
+		t.Fatalf("os.WriteFile %q want err=<nil>, got err=%v", flatFile, err)
+	}
+
+	symlinkFile := filepath.Join(sysClassNet, "bonding_masters_ln")
+	if err := os.Symlink(flatFile, symlinkFile); err != nil {
+		t.Fatalf("os.Symlink %q want err=<nil>, got err=%v", symlinkFile, err)
+	}
+
+	got, err := Interfaces()
 	if err != nil {
 		t.Fatalf("want err=<nil>, got err=%v", err)
 	}
 
-	got := make(map[string]struct{})
-	for _, iface := range ifaces {
-		got[iface.HardwareAddr.String()] = struct{}{}
-	}
-
-	want := map[string]struct{}{
-		"ce:ce:ce:ce:ce:ce": {},
-		"00:00:00:00:00:00": {},
+	want := []Interface{
+		{HardwareAddr: []byte{0xce, 0xce, 0xce, 0xce, 0xce, 0xce}}, // from eth0
+		{HardwareAddr: []byte{0xce, 0xce, 0xce, 0xce, 0xce, 0xce}}, // from eth0_ln
+		{HardwareAddr: []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00}},
 	}
 
 	if !reflect.DeepEqual(want, got) {
 		t.Fatalf("want MACs=%v, got MACs=%v", want, got)
-	}
-
-	// Ensure non-directory entry is ignored
-	if _, exists := got[""]; exists {
-		t.Fatalf("non-directory entry was incorrectly processed")
 	}
 }
